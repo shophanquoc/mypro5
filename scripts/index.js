f3397ab70050ca5f838bd7e522b952f01c504df5
NVL2001 <63655212+LAP200I@users.noreply.github.com>
2023-06-11 17:57:36 +0700

    3rd edition

--- scripts/index.js
+++ scripts/index.js
@@ -16,15 +16,69 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
-
 swiper.slideTo(3);
 
-window.onscroll = function () { scrollFunction() };
+window.onscroll = function () {
+    scrollFunction();
+};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+    ) {
         document.querySelector(".navbar").style.backgroundColor = "black";
     } else {
         document.querySelector(".navbar").style.backgroundColor = "transparent";
     }
+}
+function showAbout() {
+    // document.querySelector("#heroes").classList.add("hidden");
+    document.querySelector("#about").classList.add("show");
+    document.querySelector("#art").classList.add("show");
+    document.querySelector("#resume").classList.remove("show");
+    document.querySelector("#projects").classList.remove("show");
+    document.querySelector("#contact").classList.remove("show");
+
+}
+
+function showResume() {
+    // document.querySelector("#heroes").classList.add("hidden");
+
+    document.querySelector("#about").classList.remove("show");
+    document.querySelector("#art").classList.remove("show");
+
+    document.querySelector("#resume").classList.add("show");
+    document.querySelector("#projects").classList.remove("show");
+    document.querySelector("#contact").classList.remove("show");
+}
+
+function showProjects() {
+    // document.querySelector("#heroes").classList.add("hidden");
+
+    document.querySelector("#about").classList.remove("show");
+    document.querySelector("#art").classList.remove("show");
+
+    document.querySelector("#resume").classList.remove("show");
+    document.querySelector("#projects").classList.add("show");
+    document.querySelector("#contact").classList.remove("show");
+}
+
+function showContact() {
+    // document.querySelector("#heroes").classList.add("hidden");
+
+    document.querySelector("#about").classList.remove("show");
+    document.querySelector("#art").classList.remove("show");
+
+    document.querySelector("#resume").classList.remove("show");
+    document.querySelector("#projects").classList.remove("show");
+    document.querySelector("#contact").classList.add("show");
+}
+function showHeroes() {
+    document.querySelector("#about").classList.remove("show");
+    document.querySelector("#art").classList.remove("show");
+    document.querySelector("#contact").classList.remove("show");
+    document.querySelector("#resume").classList.remove("show");
+    document.querySelector("#projects").classList.remove("show");
+    // document.querySelector("#heroes").classList.remove("hidden");
 }
